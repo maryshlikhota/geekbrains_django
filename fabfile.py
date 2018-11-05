8b4aca97ca03aaad72c999cbf0ce5510c73f067c
Mary <prgrmmrswf@gmail.com>
2018-11-06 00:10:40 +0300

    fix fabfile

--- fabfile.py
+++ fabfile.py
@@ -6,27 +6,27 @@ from importlib import find_loader, __import__
 REQUIRENMENTS_FILE_PATH = 'requirenments.pip'
 
 def runserver(*args):
-    local(f'cd server; python manage.py runserver {" ".join(args)}')
+    local(f'cd myshop; python manage.py runserver {" ".join(args)}')
 
 
 def startapp(*args):
-    local(f'cd server python manage.py startapp {" ".join(args)}')
+    local(f'cd myshop; python manage.py startapp {" ".join(args)}')
 
 
 def makemigrations(*args):
-    local(f'cd server python manage.py makemigrations {" ".join(args)}')
+    local(f'cd myshop; python manage.py makemigrations {" ".join(args)}')
 
 
 def migrate(*args):
-    local(f'cd server python manage.py migrate {" ".join(args)}')
+    local(f'cd myshop; python manage.py migrate {" ".join(args)}')
 
 
 def collectstatic(*args):
-    local(f'cd server python manage.py collectstatic {" ".join(args)}')
+    local(f'cd myshop; python manage.py collectstatic {" ".join(args)}')
 
 
 def createsuperuser(*args):
-    local(f'cd server python manage.py createsuperuser {" ".join(args)}')
+    local(f'cd myshop; python manage.py createsuperuser {" ".join(args)}')
 
 
 def install(module_name=None, *args):
